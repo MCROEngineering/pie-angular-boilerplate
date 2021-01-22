dfb5ec28943bfb126da1fab980c95a0e78b959bc
Alex Lazar <lazar.alexandru.emil@gmail.com>
2021-01-22 12:10:18 +0200

    [fix]: lint

--- src/main.ts
+++ src/main.ts
@@ -1,9 +1,9 @@
-import { enableProdMode } from '@angular/core';
-import { platformBrowserDynamic } from '@angular/platform-browser-dynamic';
+import {enableProdMode} from '@angular/core';
+import {platformBrowserDynamic} from '@angular/platform-browser-dynamic';
 
-import { AppModule } from './app/app.module';
-import { environment } from './environments/environment';
-import { applyPolyfills, defineCustomElements } from '@pie-api/pie-api-components/dist/loader';
+import {AppModule} from './app/app.module';
+import {environment} from './environments/environment';
+import {applyPolyfills, defineCustomElements} from '@pie-api/pie-api-components/dist/loader';
 
 if (environment.production) {
   enableProdMode();
@@ -12,6 +12,6 @@ if (environment.production) {
 platformBrowserDynamic().bootstrapModule(AppModule)
   .catch(err => console.error(err));
 
-  applyPolyfills().then(() => {
-    defineCustomElements(window);
-  })
+applyPolyfills().then(() => {
+  defineCustomElements(window);
+});
